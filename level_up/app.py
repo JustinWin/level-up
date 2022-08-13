44ced9ec7173c2228af84f7dd98b49c06c21444a
Jason <13365557+jxfireruby@users.noreply.github.com>
2022-08-13 19:40:01 -0400

    Task List working. Add working. Add while running issue exists

--- level_up/app.py
+++ level_up/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template, g
+from flask import Flask, request, render_template, g, session
 
 from flask import Flask
 
@@ -55,13 +55,38 @@ def create_app(test_config=None):
         if g.user == None:
             return "Please Login"
 
-        if request.method == "POST":
-            #add task to db
-            print("POST for task")
-            print(g.user)
-            print(request.data)
+        connection = db.get_db()
+        cursor = connection.cursor()
 
-        #TODo pull tasks from DB
-        return render_template("task/task.html")
+        if request.method == "POST":
+            print("POST for task")
+            #print(request.data)
+            taskName = request.form['task-name']
+            taskTime = request.form['task-time']
+            error = None
+
+            if not taskName:
+                error = "Task name is required."
+            elif not taskTime:
+                error = "Task time is required."
+            print("her")
+            if error is None:
+                try:
+                    cursor.execute(
+                        "INSERT INTO tasks (user_id, task_name, task_time_minutes) VALUES (?, ?, ?)",
+                        (session.get("user_id"), taskName, taskTime),
+                    )
+                    cursor.commit()
+                except Exception:
+                    error = "Error"
+
+            #print(error)
+
+        tasks = cursor.execute("SELECT * FROM tasks WHERE user_id = ?", (session.get("user_id"),)).fetchall()
+        for task in tasks:   
+            print(task['user_id'], end=' ')
+            print(task['task_name'], end='')
+            print(task['task_time_minutes'])
+        return render_template("task/task.html", tasks=tasks)
 
     return app
--- level_up/schema.sql
+++ level_up/schema.sql
@@ -1,10 +1,19 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS tasks;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
+);
+
+CREATE TABLE tasks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id TEXT NOT NULL,
+  task_name TEXT NOT NULL,
+  task_time_minutes INTEGER NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
 --TODO delete this

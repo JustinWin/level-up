816116ee2d5a5b9b64d7fbc2b562e256472cf70e
JustinWIn <42788541+JustinWIn@users.noreply.github.com>
2022-09-08 00:45:53 -0400

    Refactored play timer functionality

--- level_up/static/js/task.js
+++ level_up/static/js/task.js
@@ -1,6 +1,3 @@
-
-add_delete_funtionality_to_all_cards();
-display_time_on_refresh();
 // POST Function for deleting tasks
 async function postRequest(url, data) {
     const response = await fetch(url, {
@@ -15,8 +12,12 @@ async function postRequest(url, data) {
     return responseJson
 }
 // Generic POST function for adding tasks
-async function addTaskPost(url, data) {
+async function add_task_post(url, data) {
     r = await postRequest('/task', data).then(responseJson => {
+
+        // Convert total seconds to readable format
+        let [hours, minutes, seconds] = convert_seconds_to_HMS(responseJson["total-seconds"]);
+        let total_seconds = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 
         // Create card element
         let card = document.createElement("div")
@@ -36,7 +37,7 @@ async function addTaskPost(url, data) {
                         <span class="mdc-linear-progress__bar-inner"></span>
                     </div>
                 </div>
-                <p id="total-time-${responseJson["task-id"]}" class="p-1">00:00:00</p>
+                <p id="total-time-${responseJson["task-id"]}" class="p-1">${total_seconds}</p>
             </div>
 
             <div class="controlls">
@@ -47,10 +48,9 @@ async function addTaskPost(url, data) {
         // Append card to task container
         let taskListContainer = document.getElementById("task-list-container");
         taskListContainer.append(card);
-        play_pause_display(
-            responseJson['task-id'],
-            responseJson['total-seconds']
-        );
+        const pause_btn = document.getElementById(`play-pause-${responseJson["task-id"]}`);
+        pause_btn.addEventListener('click', function () { timer_controls(responseJson['task-id'], responseJson['total-seconds']) });
+
 
     })
     add_delete_funtionality_to_all_cards();
@@ -68,48 +68,6 @@ function convert_seconds_to_HMS(input_seconds) {
 
     return [hours, minutes, seconds]
 }
-document.getElementById("add-task-submit-button").addEventListener("click", function () {
-    // Prevents refresh
-    event.preventDefault();
-    taskName = document.getElementById("task-name").value;
-    taskTimeHours = document.getElementById("task-time-hours").value;
-    taskTimeMinutes = document.getElementById("task-time-minutes").value;
-    taskTimeSeconds = document.getElementById("task-time-seconds").value;
-    addTaskPost(
-        '/task',
-        {
-            'event': 'add',
-            'task-name': taskName,
-            'total-seconds': convert_HMS_to_seconds(taskTimeHours, taskTimeMinutes, taskTimeSeconds)
-        });
-});
-
-
-
-let snackbar_dismiss = document.getElementById("snackbar-dismiss")
-snackbar_dismiss.addEventListener("click", function () {
-    let snackbar_delete = document.getElementById("snackbar-delete");
-    snackbar_delete.classList.remove("mdc-snackbar--open", "show")
-    snackbar_delete.classList.add('mdc-snackbar--close');
-});
-
-let snackbar_undo = document.getElementById("snackbar-undo")
-snackbar_undo.addEventListener("click", (e) => {
-    recently_deleted_task = (postRequest('/task', { 'event': 'undo' }).then(responseJson => {
-        addTaskPost(
-            '/task',
-            {
-                'event': 'add',
-                'task-name': responseJson["task-name"],
-                'total-seconds': responseJson["total-seconds"]
-            }
-        );
-    }));
-    let snackbar_delete = document.getElementById("snackbar-delete");
-    snackbar_delete.classList.remove("mdc-snackbar--open", "show")
-    snackbar_delete.classList.add('mdc-snackbar--close');
-
-});
 
 
 
@@ -128,103 +86,95 @@ function add_delete_funtionality_to_all_cards() {
     })
 }
 
-// On page refresh display times
-function display_time_on_refresh() {
-    all_tasks = postRequest('/task', { 'event': 'display' }).then(data => {
-        all_tasks_ids = Object.keys(data);
-        for (let i = 0; i < all_tasks_ids.length; i++) {
-            task = (data[all_tasks_ids[i]]);
-            play_pause_display(
-                task["id"],
-                task["total_seconds"]
-            );
-        }
-    })
+// // On page refresh display times
+// function display_time_on_refresh() {
+//     all_tasks = postRequest('/task', { 'event': 'display' }).then(data => {
+//         all_tasks_ids = Object.keys(data);
+//         for (let i = 0; i < all_tasks_ids.length; i++) {
+//             task = (data[all_tasks_ids[i]]);
+//             initialize_timer(
+//                 task["id"],
+//                 task["total_seconds"]
+//             );
+//         }
+//     })
 
 
-}
+// }
 
-// Parent function that add makes the timer work
-function play_pause_display(id, totalTime) {
-    const progressBar = document.getElementById(`progressbar-${id}`);
-    const elasped_time_div = document.getElementById(`elasped-time-${id}`);
-    const total_time_div = document.getElementById(`total-time-${id}`);
+function initialize_timer(id, total_time) {
+
     const pause_btn = document.getElementById(`play-pause-${id}`);
-
-    let interval_timer;
-    let is_paused = false;
-    let is_ticking = false;
+    const elasped_time_div = document.getElementById(`elasped-time-${id}`);
     let elasped_seconds_interval = 0;
 
-    let [hours, minutes, seconds] = convert_seconds_to_HMS(totalTime);
+    interval_timer = setInterval(function () {
+        elasped_seconds_interval++;
 
-    // Display total time
-    total_time_div.textContent = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        // Update elasped time
+        let [elasped_hours, elasped_minutes, elasped_seconds] = convert_seconds_to_HMS(elasped_seconds_interval);
+        let elaspedTime = `${elasped_hours < 10 ? '0' : ''}${elasped_hours}:${elasped_minutes < 10 ? '0' : ''}${elasped_minutes}:${elasped_seconds < 10 ? '0' : ''}${elasped_seconds}`;
+        elasped_time_div.textContent = elaspedTime;
 
-    function timer() {
-        interval_timer = setInterval(function () {
-            elasped_seconds_interval++;
+        // When timer completed
+        if (elasped_seconds_interval >= total_time) {
+            console.log(interval_timer)
 
-            // Update elasped time
-            let [elasped_hours, elasped_minutes, elasped_seconds] = convert_seconds_to_HMS(elasped_seconds_interval);
-            let elaspedTime = `${elasped_hours < 10 ? '0' : ''}${elasped_hours}:${elasped_minutes < 10 ? '0' : ''}${elasped_minutes}:${elasped_seconds < 10 ? '0' : ''}${elasped_seconds}`;
-            elasped_time_div.textContent = elaspedTime;
-
-            // When timer completed
-            if (elasped_seconds_interval >= totalTime) {
-                clearInterval(interval_timer);
-                is_ticking = false;
-
-                // Change pause button to play button
-                pause_btn.classList.remove('pause');
-                pause_btn.classList.add('play');
-
-                // Display task completed modal
-                display_task_completed(totalTime);
-
-                // Update user's experience
-                postRequest('/task', { 'event': 'update', 'exp': totalTime });
-
-                // TODO 
-                return;
-            }
-        }, 1000);
-    }
-    function timer_controls() {
-
-        // Timer is started
-        if (is_ticking === false) {
-            timer();
-            is_ticking = true;
-            this.classList.remove('play');
-            this.classList.add('pause');
-
-            // Timer is unpaused
-        } else if (is_paused) {
-            this.classList.remove('play');
-            this.classList.add('pause');
-            timer();
-            is_paused = is_paused ? false : true
-
-            // Timer is paused
-        } else {
-            this.classList.remove('pause');
-            this.classList.add('play');
             clearInterval(interval_timer);
-            is_paused = is_paused ? false : true;
-        }
-    }
 
-    pause_btn.addEventListener('click', timer_controls);
+            console.log(interval_timer)
+            is_ticking = false;
+
+            // Change pause button to play button
+            pause_btn.classList.remove('pause');
+            pause_btn.classList.add('play');
+
+            // Display task completed modal
+            display_task_completed(total_time);
+
+            // Update user's experience
+            postRequest('/task', { 'event': 'update', 'exp': total_time });
+            ;
+        }
+    }, 1000);
+
+    return interval_timer;
 }
 
+
+function timer_controls(id, total_time) {
+
+    const progressBar = document.getElementById(`progressbar-${id}`);
+    const pause_btn = document.getElementById(`play-pause-${id}`);
+
+    let is_paused = false;
+
+    // Timer is unpaused
+    if (pause_btn.classList.contains("play")) {
+        pause_btn.classList.remove('play');
+        pause_btn.classList.add('pause');
+        current_interval = initialize_timer(id, total_time);
+        is_paused = is_paused ? false : true
+
+    } else {
+        // Timer is paused
+        pause_btn.classList.remove('pause');
+        pause_btn.classList.add('play');
+        console.log(interval_timer);
+        clearInterval(current_interval);
+        is_paused = is_paused ? false : true;
+    }
+}
+
+
+
 // Displays a modal to the user showing their experience gained for task completion
-function display_task_completed(totalTime) {
+function display_task_completed(total_time) {
     let task_complete_modal = new bootstrap.Modal(document.getElementById('task-complete-modal'), {
         keyboard: false
     });
 
-    document.getElementById("exp-gain").innerHTML = "+ " + totalTime + " XP";
+    document.getElementById("exp-gain").innerHTML = "+ " + total_time + " XP";
     task_complete_modal.show();
     function close_modal() {
         let close_modal_backdrop = $(".modal-backdrop");
@@ -240,3 +190,53 @@ function display_task_completed(totalTime) {
 
     content_container.append(modal_backdrop);
 }
+
+document.getElementById("add-task-submit-button").addEventListener("click", function () {
+    // Prevents refresh
+    event.preventDefault();
+    taskName = document.getElementById("task-name").value;
+    taskTimeHours = document.getElementById("task-time-hours").value;
+    taskTimeMinutes = document.getElementById("task-time-minutes").value;
+    taskTimeSeconds = document.getElementById("task-time-seconds").value;
+    add_task_post(
+        '/task',
+        {
+            'event': 'add',
+            'task-name': taskName,
+            'total-seconds': convert_HMS_to_seconds(taskTimeHours, taskTimeMinutes, taskTimeSeconds)
+        });
+});
+
+function initialize_snackbar() {
+    let snackbar_dismiss = document.getElementById("snackbar-dismiss")
+    snackbar_dismiss.addEventListener("click", function () {
+        let snackbar_delete = document.getElementById("snackbar-delete");
+        snackbar_delete.classList.remove("mdc-snackbar--open", "show")
+        snackbar_delete.classList.add('mdc-snackbar--close');
+    });
+
+    let snackbar_undo = document.getElementById("snackbar-undo")
+    snackbar_undo.addEventListener("click", (e) => {
+        recently_deleted_task = (postRequest('/task', { 'event': 'undo' }).then(responseJson => {
+            add_task_post(
+                '/task',
+                {
+                    'event': 'add',
+                    'task-name': responseJson["task-name"],
+                    'total-seconds': responseJson["total-seconds"]
+                }
+            );
+        }));
+        let snackbar_delete = document.getElementById("snackbar-delete");
+        snackbar_delete.classList.remove("mdc-snackbar--open", "show")
+        snackbar_delete.classList.add('mdc-snackbar--close');
+
+    });
+}
+
+// TODO make it so only one interval can go at a time (other ones grayed out)
+let current_interval;
+
+add_delete_funtionality_to_all_cards();
+initialize_snackbar();
+// display_time_on_refresh();
